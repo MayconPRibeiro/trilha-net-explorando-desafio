721d8f43a508c29825dac2982b0c645cf7206c7a
MayconPRibeiro <maycon_ribeiro10@hotmail.com>
2024-05-16 14:56:54 -0300

    Atualizado com as implementações necessarias

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -16,15 +16,18 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // Implementado e testado
+            int totalHospedes = hospedes.Count();
+            if (Suite.Capacidade >= totalHospedes)
             {
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // Implementado e testado
+                throw new Exception("A quantidade de hóspedes não pode exceder a capacidade da suíte");
+
             }
         }
 
@@ -36,22 +39,24 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // Implementado e testado
+            return Hospedes.Count();
+
         }
 
         public decimal CalcularValorDiaria()
         {
+
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            // Implementado e testado
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // Implementado e testado
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor = valor * 0.90M;
             }
 
             return valor;
